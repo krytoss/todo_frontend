fe8c7cac4098ce0c9d5770133ee6758ee294064f
BuildTools <kopcsanyiludovit@gmail.com>
2024-03-20 16:37:31 +0100

    very first api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:8080/tasks')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setTodoList(data)
+      })
+  }, [ setTodoList ])
 
   return (
     <div className="App">
